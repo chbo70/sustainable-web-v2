5ba4a796541f81bac3e73f09efb0c92779c3663b
Boon-Chung Chi <boon.chung.chi@gmx.at>
2025-05-02 19:27:51 +0200

    applied G2.01

--- src/components/videosection/VideoSection.jsx
+++ src/components/videosection/VideoSection.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import "./videosection.css";
-import VideoFile from "../../assets/videos/forest_high.mp4";
+import VideoFile from "../../assets/videos/forest_high.webm";
 
 const VideoSection = () => {
   const videoRef = useRef(null);
